160f4905e2adee48866145af60e11e601af51624
Gonzalo Vasco López <gvascolopez@gmail.com>
2025-04-18 17:03:45 +0200

    feat: added pdf export and scrollbar styles

--- docudev-front/src/components/Docus/CreateDocu/CreateDocu.tsx
+++ docudev-front/src/components/Docus/CreateDocu/CreateDocu.tsx
@@ -6,28 +6,49 @@ import { BlockNoteView } from '@blocknote/shadcn'
 import { es } from '@blocknote/core/locales'
 import { en } from '@blocknote/core/locales'
 import { codeBlock } from 'constants/editor'
+import { jsPDF } from 'jspdf'
+import html2canvas from 'html2canvas'
+import { useRef } from 'react'
 import '@blocknote/core/fonts/inter.css'
 import '@blocknote/shadcn/style.css'
 import './CreateDocu.css'
+import Button from 'components/elements/Button/Button'
 
 const CreateDocu = () => {
 	const { t, i18n } = useTranslation()
 	const dictionary = i18n.language.startsWith('es') ? es : en
+	const editorRef = useRef(null)
 
 	const editor = useCreateBlockNote({
 		dictionary,
 		codeBlock
 	})
 
+	const exportToPdf = async () => {
+		if (!editorRef.current) return
+		const editorElement = editorRef.current
+		const canvas = await html2canvas(editorElement, {
+			scale: 5,
+			useCORS: true
+		})
+		const imgData = canvas.toDataURL('image/png')
+		const imgWidth = 210
+		const imgHeight = (canvas.height * imgWidth) / canvas.width
+		const pdf = new jsPDF('p', 'mm', [imgWidth, imgHeight])
+		pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight)
+		pdf.save('documento.pdf')
+	}
+
 	return (
 		<DashboardLayout>
-			<div className='create_docu-header'>
-				<Header title={t('create_docu.title')} />
+			<div className='create-docu-header'>
+				<Header title={t('create_docu.title')} />{' '}
+				<Button onClick={exportToPdf}>Exportar a PDF</Button>
 			</div>
 			<div className='create_docu-container'>
 				<h2>{t('create_docu.subtitle')}</h2>
 				<div className='create_docu-editor'>
-					<BlockNoteView editor={editor} />
+					<BlockNoteView editor={editor} ref={editorRef} />
 				</div>
 			</div>
 		</DashboardLayout>
--- docudev-front/src/constants/editor.ts
+++ docudev-front/src/constants/editor.ts
@@ -4,81 +4,309 @@ export const codeBlock = {
 	indentLineWithTab: true,
 	defaultLanguage: 'typescript',
 	supportedLanguages: {
-		typescript: {
-			name: 'TypeScript',
-			aliases: ['ts']
-		},
-		javascript: {
-			name: 'JavaScript',
-			aliases: ['js']
-		},
-		php: {
-			name: 'PHP',
+		abap: {
+			name: 'ABAP',
 			aliases: []
 		},
-		tsx: {
-			name: 'TSX',
-			aliases: ['typescriptreact']
+		ada: {
+			name: 'Ada',
+			aliases: []
 		},
-		python: {
-			name: 'Python',
-			aliases: ['py']
+		apex: {
+			name: 'Apex',
+			aliases: []
+		},
+		asciidoc: {
+			name: 'AsciiDoc',
+			aliases: ['adoc']
+		},
+		asm: {
+			name: 'Assembly',
+			aliases: []
+		},
+		astro: {
+			name: 'Astro',
+			aliases: []
+		},
+		bash: {
+			name: 'Bash',
+			aliases: ['sh', 'shell', 'bash']
+		},
+		c: {
+			name: 'C',
+			aliases: []
+		},
+		clojure: {
+			name: 'Clojure',
+			aliases: ['clj']
+		},
+		cmake: {
+			name: 'CMake',
+			aliases: []
+		},
+		cobol: {
+			name: 'COBOL',
+			aliases: []
+		},
+		coffeescript: {
+			name: 'CoffeeScript',
+			aliases: ['coffee']
+		},
+		cpp: {
+			name: 'C++',
+			aliases: ['cc', 'c++']
+		},
+		crystal: {
+			name: 'Crystal',
+			aliases: []
+		},
+		csharp: {
+			name: 'C#',
+			aliases: ['cs', 'csharp']
 		},
 		css: {
 			name: 'CSS',
+			aliases: []
+		},
+		dart: {
+			name: 'Dart',
+			aliases: []
+		},
+		dockerfile: {
+			name: 'Dockerfile',
+			aliases: ['docker']
+		},
+		elixir: {
+			name: 'Elixir',
+			aliases: []
+		},
+		elm: {
+			name: 'Elm',
+			aliases: []
+		},
+		erlang: {
+			name: 'Erlang',
+			aliases: ['erl']
+		},
+		fortran: {
+			name: 'Fortran',
+			aliases: ['f', 'f77', 'f90', 'f95', 'f03', 'f08']
+		},
+		fsharp: {
+			name: 'F#',
+			aliases: ['fs']
+		},
+		go: {
+			name: 'Go',
+			aliases: ['golang']
+		},
+		graphql: {
+			name: 'GraphQL',
+			aliases: ['gql']
+		},
+		groovy: {
+			name: 'Groovy',
+			aliases: []
+		},
+		haskell: {
+			name: 'Haskell',
+			aliases: ['hs']
+		},
+		hcl: {
+			name: 'HCL',
 			aliases: []
 		},
 		html: {
 			name: 'HTML',
 			aliases: ['htm']
 		},
-		json: {
-			name: 'JSON',
+		ini: {
+			name: 'INI',
 			aliases: []
 		},
 		java: {
 			name: 'Java',
 			aliases: []
 		},
-		c: {
-			name: 'C',
+		javascript: {
+			name: 'JavaScript',
+			aliases: ['js']
+		},
+		json: {
+			name: 'JSON',
 			aliases: []
 		},
-		cpp: {
-			name: 'C++',
-			aliases: ['cc', 'c++']
+		julia: {
+			name: 'Julia',
+			aliases: ['jl']
 		},
-		csharp: {
-			name: 'C#',
-			aliases: ['cs', 'csharp']
+		kotlin: {
+			name: 'Kotlin',
+			aliases: ['kt', 'kts']
 		},
-		ruby: {
-			name: 'Ruby',
-			aliases: ['rb']
+		latex: {
+			name: 'LaTeX',
+			aliases: ['tex']
 		},
-		go: {
-			name: 'Go',
-			aliases: ['golang']
+		less: {
+			name: 'Less',
+			aliases: []
 		},
-		rust: {
-			name: 'Rust',
-			aliases: ['rs']
+		liquid: {
+			name: 'Liquid',
+			aliases: []
 		},
-		bash: {
-			name: 'Bash',
-			aliases: ['sh', 'shell', 'bash']
+		lua: {
+			name: 'Lua',
+			aliases: []
+		},
+		markdown: {
+			name: 'Markdown',
+			aliases: ['md']
+		},
+		matlab: {
+			name: 'MATLAB',
+			aliases: []
+		},
+		mermaid: {
+			name: 'Mermaid',
+			aliases: ['mmd']
+		},
+		nix: {
+			name: 'Nix',
+			aliases: []
+		},
+		objectivec: {
+			name: 'Objective-C',
+			aliases: ['objc', 'objective-c', 'objective-cpp']
+		},
+		ocaml: {
+			name: 'OCaml',
+			aliases: []
+		},
+		pascal: {
+			name: 'Pascal',
+			aliases: []
+		},
+		perl: {
+			name: 'Perl',
+			aliases: []
+		},
+		php: {
+			name: 'PHP',
+			aliases: []
 		},
 		powershell: {
 			name: 'PowerShell',
 			aliases: ['ps', 'ps1', 'powershell']
 		},
+		prisma: {
+			name: 'Prisma',
+			aliases: []
+		},
+		prolog: {
+			name: 'Prolog',
+			aliases: []
+		},
+		pug: {
+			name: 'Pug',
+			aliases: ['jade']
+		},
+		python: {
+			name: 'Python',
+			aliases: ['py']
+		},
+		r: {
+			name: 'R',
+			aliases: []
+		},
+		raku: {
+			name: 'Raku',
+			aliases: ['perl6']
+		},
+		regex: {
+			name: 'Regex',
+			aliases: ['regexp']
+		},
+		ruby: {
+			name: 'Ruby',
+			aliases: ['rb']
+		},
+		rust: {
+			name: 'Rust',
+			aliases: ['rs']
+		},
+		scala: {
+			name: 'Scala',
+			aliases: []
+		},
+		scheme: {
+			name: 'Scheme',
+			aliases: []
+		},
+		scss: {
+			name: 'SCSS',
+			aliases: []
+		},
+		solidity: {
+			name: 'Solidity',
+			aliases: []
+		},
+		sql: {
+			name: 'SQL',
+			aliases: []
+		},
+		stylus: {
+			name: 'Stylus',
+			aliases: ['styl']
+		},
+		svelte: {
+			name: 'Svelte',
+			aliases: []
+		},
+		swift: {
+			name: 'Swift',
+			aliases: []
+		},
+		terraform: {
+			name: 'Terraform',
+			aliases: ['tf', 'tfvars']
+		},
+		toml: {
+			name: 'TOML',
+			aliases: []
+		},
+		tsx: {
+			name: 'TSX',
+			aliases: ['typescriptreact']
+		},
+		twig: {
+			name: 'Twig',
+			aliases: []
+		},
+		typescript: {
+			name: 'TypeScript',
+			aliases: ['ts']
+		},
+		vue: {
+			name: 'Vue',
+			aliases: ['vue-html']
+		},
+		wasm: {
+			name: 'WebAssembly',
+			aliases: []
+		},
+		xml: {
+			name: 'XML',
+			aliases: []
+		},
 		yaml: {
 			name: 'YAML',
 			aliases: ['yml']
 		},
-		markdown: {
-			name: 'Markdown',
-			aliases: ['md']
+		zig: {
+			name: 'Zig',
+			aliases: []
 		}
 	},
 	createHighlighter: () =>
